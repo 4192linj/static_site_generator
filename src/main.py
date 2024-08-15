13252df9599356da8698d61123d3de1e6ccf98db
4192linj <jieqinglin@hotmail.com>
2024-08-15 17:59:38 -0400

    create TextNode class in textnode.py and test_textnode.py to unit test, also the shell files to execute the py files

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    t = TextNode("Hello", "bold", "www.123.com")
+    print(t)
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        n_1 = TextNode("Hello", "Bold", "www.123.com")
+        n_2 = TextNode("Hello", "Bold", "www.123.com")
+        self.assertEqual(n_1,n_2)
+    
+    def test_not_eq(self):
+        n_1 = TextNode("Hello", "Bold", "www.456.com")
+        n_2 = TextNode("Hiya", "Italic", "www.123.com")
+        self.assertNotEqual(n_1,n_2)
+    
+    def test_none_case(self):
+        n_1 = TextNode("Hello", "Bold", "www.456.com")
+        n_2 = None
+        self.assertNotEqual(n_1,n_2)
+
+    def test_create_node(self):
+        n_1 = TextNode("Hello", "Bold", "www.123.com")
+        self.assertEqual(n_1.url, "www.123.com")
+
+if __name__ == "__main__":
+    unittest.main()
